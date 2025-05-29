a797a1b4430a3804b70597ec24d93fc66b067985
lady.program <nymeriastark74@gmail.com>
2025-05-29 10:37:31 +0200

    Initial Commit

---
+++ MiAppTests/MiAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiAppTests.swift
+//  MiAppTests
+//
+//  Created by Mañanas on 29/5/25.
+//
+
+import Testing
+@testable import MiApp
+
+struct MiAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
