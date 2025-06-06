8a18008c37cb82734066ae2bee8153b16b3fe8ad
lady.program <nymeriastark74@gmail.com>
2025-06-06 11:19:25 +0200

    2nd version app, log in Google

--- MiApp/SignInViewController.swift
+++ MiApp/SignInViewController.swift
@@ -6,7 +6,10 @@
 //
 
 import UIKit
+import FirebaseCore
 import FirebaseAuth
+import GoogleSignIn
+
 
 class SignInViewController: UIViewController {
     
@@ -62,6 +65,48 @@ class SignInViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func signInGoogle(_ sender: Any) {
+        // Configure Google SignIn with Firebase
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            guard let user = result?.user, let idToken = user.idToken?.tokenString else {
+                print("Cannot get user token from Google")
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+
+            Auth.auth().signIn(with: credential) { [unowned self] result, error in
+                if let error = error {
+                    // Hubo un error
+                    print(error)
+                    
+                    let alertController = UIAlertController(title: "Sign In", message: error.localizedDescription, preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .default))
+                    self.present(alertController, animated: true, completion: nil)
+                } else {
+                    // Todo correcto
+                    print("User signs in successfully")
+                    
+                    goToHome()
+                }
+            }
+        }
+    }
+    
+    
     
     func goToHome() {
         self.performSegue(withIdentifier: "goToHome", sender: nil)
