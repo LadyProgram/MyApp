9a8bda8b5509b469dc20a0db427239632ad674f6
lady.program <nymeriastark74@gmail.com>
2025-06-10 12:40:44 +0200

    4th version menus profile, settings, home

---
+++ MiApp/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  MiApp
+//
+//  Created by Mañanas on 10/6/25.
+//
+
+import UIKit
+import FirebaseAuth
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signOut(_ sender: Any) {
+        do {
+          try Auth.auth().signOut()
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
+        self.navigationController?.navigationController?.popToRootViewController(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
